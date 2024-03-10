6bba4ee11f277e018dba1a1ec18c1eb0ace7024e
nisaaja-panggil <nisa31102007@gmail.com>
2024-03-10 15:03:47 +0700

    membuat customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -11,12 +11,39 @@ class CustomerController extends Controller
     //
     public function index()
     {
-        return view('castomer.tabel', [
+        return view('customer.tabel', [
             "title" => "customer",
             "data" => Customer::all()
         ]);
     }
     public function create():view{
         return view('customer.tambah')->with(["title"=>"tambah data customer"]);
+    }
+    public function store(Request $request):RedirectResponse{
+        $request->validate([
+            "name"=>"required",
+            "email"=>"required",
+            "phone"=>"required",
+            "address"=>"nullable"
+        ]);
+       Customer::create($request->all());
+       return redirect()->route('pelanggan.index')->with('success data','data customer berhasil ditambahkan');
+    }
+    public function edit(Customer $pelanggan):view{
+        return view('customer.edit',compact('pelanggan'))->with(["title"=>"ubah data customer"]);
+    }
+    public function update(Request $request, customer $pelanggan):RedirectResponse{
+        $request->validate([
+            "name"=>"required",
+            "email"=>"required",
+            "phone"=>"required",
+            "address"=>"nullable"
+        ]);
+       $pelanggan->update($request->all());
+       return redirect()->route('pelanggan.index')->with('updated','data pelanggan berhasil di ubah');
+    }
+    public function show(Customer $pelanggan):view{
+        return view('customer.tampil',compact('pelanggan'))
+                          ->with(["title"=>"data customer"]);
     }
 }
